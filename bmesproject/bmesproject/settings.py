6f9248ce865a4c008f172193d7be441f3b3078fe
Aya Tantaoui <ayatantaoui12@gmail.com>
2026-04-27 17:24:49 +0100

    adding catalogue app to settings

--- bmesproject/bmesproject/settings.py
+++ bmesproject/bmesproject/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'app',#addig app to settings
+    'catalogueapp',#adding catalogue app
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
