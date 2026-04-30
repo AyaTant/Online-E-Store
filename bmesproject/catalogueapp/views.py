5f1088dbab2210f14e0ec6ea9855c1d2d75a1ba3
Aya Tantaoui <ayatantaoui12@gmail.com>
2026-04-30 19:36:46 +0100

    updating the catalogue view with the service

--- bmesproject/catalogueapp/views.py
+++ bmesproject/catalogueapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpRequest
 from datetime import datetime
+from catalogueapp import catalogue_service
 # Create your views here.
 def catalogue(request, category_slug='all-categories',brand_slug='all-brands'):
     assert isinstance(request,HttpRequest)
@@ -14,12 +15,14 @@ def catalogue(request, category_slug='all-categories',brand_slug='all-brands'):
             }
         )
     else:
+        page_object = catalogue_service.fetch_products(request,category_slug,brand_slug)
         return render(
             request,
             'catalogueapp/index.html',
             {
                 'title':'Product Page',
                 'year':datetime.now().year,
+                'page_object':page_object,
             }
         )
 
