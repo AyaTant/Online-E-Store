5645a4775720953b0d1d79d4a61cd99e75d749c4
Aya Tantaoui <ayatantaoui12@gmail.com>
2026-04-30 19:08:54 +0100

    product catalogue +product detail views added

--- bmesproject/catalogueapp/views.py
+++ bmesproject/catalogueapp/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
-
+from django.http import HttpRequest
+from datetime import datetime
 # Create your views here.
+def catalogue(request, category_slug='all-categories',brand_slug='all-brands'):
+    assert isinstance(request,HttpRequest)
+    if request.method == "POST":
+        return render (
+            request,
+            'catalogueapp/index.html',
+            {
+                'title':'Product Page',
+                'year':datetime.now().year,
+            }
+        )
+    else:
+        return render(
+            request,
+            'catalogueapp/index.html',
+            {
+                'title':'Product Page',
+                'year':datetime.now().year,
+            }
+        )
+
+def product_detail(args):
+    return render()
