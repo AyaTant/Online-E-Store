3bfc1a72c3e1e901d31026aff0eb4c187fad2735
Aya Tantaoui <ayatantaoui12@gmail.com>
2026-04-30 19:41:39 +0100

    updating the bmes context servixe with list of categories and brands

--- bmesproject/app/bmes_context_service.py
+++ bmesproject/app/bmes_context_service.py
@@ -1,5 +1,8 @@
 from bmesproject.bmesproject import settings
+from catalogueapp.models import Brand, Category
 def bmes_context(request):
     return {
         'site_name': settings.SITE_NAME,
+        'categories':Category.objects.filter(category_status=0),
+        'brands':Brand.objects.filter(brand_status=0)
     }
