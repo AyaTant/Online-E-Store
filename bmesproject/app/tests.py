5009f9a9a9ca68422b01f4e9da3b46e4bb416573
Aya Tantaoui <ayatantaoui12@gmail.com>
2026-05-04 13:34:54 +0100

    feat: tests.py file  for app created

--- bmesproject/app/tests.py
+++ bmesproject/app/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
-
+import django
 # Create your tests here.
+class ViewTest(TestCase):
+    if django.VERSION[:2] >=(1,7):
+        @classmethod
+        def setUpClass(cls):
+            super(ViewTest,cls).setUpClass()
+            django.setup()
+    
+    def test_home(self):
+        """Tests the home page"""
+        response=self.client.get('/')
+        self.assertContains(response,'Home Page',1,200)
+    
+    def test_contact(self):
+        """Tests the contact page"""
+        response = self.client.get('/contact')
+        self.assertContains(response,'Contact',3,200)
+    
+    def test_about(self):
+        """Tests the about page"""
+        response=self.client.get('/about')
+        self.assertContains(response,'About',3,200)
