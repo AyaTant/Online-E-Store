3a6cbd51f906c014ccfd27f762174023732fb0e1
Aya Tantaoui <ayatantaoui12@gmail.com>
2026-05-22 13:31:07 +0100

    fix: updating bmes_context_service.py

--- bmesproject/app/bmes_context_service.py
+++ bmesproject/app/bmes_context_service.py
@@ -4,5 +4,7 @@ def bmes_context(request):
     return {
         'site_name': settings.SITE_NAME,
         'categories':Category.objects.filter(category_status=0),
-        'brands':Brand.objects.filter(brand_status=0)
+        'brands':Brand.objects.filter(brand_status=0),
+        'selected_category':'all-categories',
+        'selected_brand':'all-brands',
     }
