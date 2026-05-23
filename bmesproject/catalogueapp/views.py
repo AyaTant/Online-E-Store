7517939ee6b3b0c7cd27a6b765a4937d2ee91bfd
Aya Tantaoui <ayatantaoui12@gmail.com>
2026-05-23 17:09:50 +0100

    fix: changing catalogueapp views.py file

--- bmesproject/catalogueapp/views.py
+++ bmesproject/catalogueapp/views.py
@@ -2,10 +2,14 @@ from django.shortcuts import render
 from django.http import HttpRequest
 from datetime import datetime
 from catalogueapp import catalogue_service
+from cartapp import cart_service
 # Create your views here.
 def catalogue(request, category_slug='all-categories',brand_slug='all-brands'):
     assert isinstance(request,HttpRequest)
     if request.method == "POST":
+        cart_service.add_to_cart(request) # adding items to the cart
+
+        # the code to add items to the cart
         page_object = catalogue_service.fetch_products(request,category_slug,brand_slug)
         return render (
             request,
